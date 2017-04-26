d245d4d92a73a72009f9175b90d17b35a045fc51
Mitchell Blom <mitchellblom@gmail.com>
2017-04-25 19:27:28 -0500

    class notes about writing api result details to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
 
-const apiKey = "";				// key goes here
-let resultNames;
+const apiKey = '';								// key goes here
 
-$("body").on("click", "li", (e) => {
-	// console.log(e.target.innerHTML);
-	loadPlaces(e.target.innerHTML).then((data) => {
-		results = data.results;
-			writeToDom(results);
+	$('body').on('click', 'li', (e) => {
+		loadPlaces(e.target.innerHTML).then((data) => {
+			results = data.results;
+				writePlaceToDom(results);
+			})
+		.catch((error) => {
+			console.log(error);
 		})
-	.catch((error) => {
-		console.log(error);
-	})
-});
+	});
+
+	$('body').on('click', '.place', (e) => {
+		let place_id = e.target.id;
+		loadDetail(place_id).then((result) => {
+			writeAddressToDom(result.formatted_address);
+		});
+	});
+
+	const loadDetail = (place_id) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/details/json?
+				placeid=${place_id}&key=${apiKey}
+			`)
+			.done((data) => {
+				resolve(data.result)		// this result is getting back to line 17 for the then()
+			})
+			.fail((error) => {
+				reject(error)
+			});
+		});
+	};
 
 	const loadPlaces = (dropdownType) => {
 		return new Promise ((resolve, reject) => {
@@ -24,13 +43,23 @@ $("body").on("click", "li", (e) => {
 		});
 	};
 
-	const writeToDom = (results) => {
-		results.forEach((each)=> {
-			let string = `<p>${each.name}</p>`;
-			$("#name-div").append(string);
+	const writeAddressToDom = (address) => {
+			let string = `<div>${address}</div>`;
+			$('#address-div').html(string);
+	}
 
-	});
+	const writePlaceToDom = (placeName) => {
+		placeName.forEach((each) => {
+			let string = `<a href="#"><div id='${each.place_id}' class='place'>${each.name}</div></a>`;
 
+		// let string = ""									// alternative using for loop instead of forEach
+		// for (let i = 0; i < placeName.length; i++) {
+		// 	string += `<a href="#"><div id='${each.place_id}' class='place'>${each.name}</div></a>`;
+		// }
+
+			$('#name-div').append(string);
+
+		});
 	}
 
 
